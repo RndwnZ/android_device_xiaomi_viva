c8e9c424c42d135f0c54ef3581cf93e59e4195e2
Zan <122100768+RndwnZ@users.noreply.github.com>
2023-08-25 17:57:21 +0000

    viva: Inherit from common tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,13 @@
 #
 # Copyright (C) 2022 ArrowOS
 # Copyright (C) 2023 LineageOS
+# Copyright (C) 2023 SuperiorExtended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_viva.mk
+    $(LOCAL_DIR)/superior_viva.mk
 
 COMMON_LUNCH_CHOICES := \
-    $(foreach FLAVOR, user userdebug eng, lineage_viva-$(FLAVOR))
+    $(foreach FLAVOR, user userdebug eng, superior_viva-$(FLAVOR))
